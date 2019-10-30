b0af33b9d7909a96428a86bb6de7de5f9be08811
Izaias Neto <izaiasneto4@gmail.com>
2019-10-30 16:51:25 -0300

    [feat] Added logic to showing, reseting and updating the board

--- bin/main.rb
+++ bin/main.rb
@@ -90,8 +90,8 @@ module TicTacToe
     def game
       
       this = GameEngine.new
-      this.show_board
-
+      # this.update_board(10, 3)
+      this.update_board(0, 8)
     end
 
     def Player1Name
@@ -108,51 +108,60 @@ class GameEngine
   attr_accessor :board
   
   def initialize
-    @board = [
-      [1, 2, 3],
-      [4, 10, 6],
-      [7, 8, 9]    ]
+    @board = [1, 2, 3, 4, 10, 6, 7, 8, 9]
   end
   # player1 gets X and start first
   # player2 gets zero
 
-  def show_board
-    @board.each {|row| puts "| #{row[0]==10? 'X': row[0]} |  | #{row[1]==10? 'X': row[1]} | | #{row[2]==10? 'X': row[2]} |"}
+  def show_board    
+    p " #{ @board[0] == 10? 'X' : @board[0] } | #{@board[1] == 10? 'X' : @board[1] } | #{ @board[2]== 10? 'X' : @board[2] }"
+    p " #{ @board[3] == 10? 'X' : @board[3] } | #{@board[4] == 10? 'X' : @board[4]} | #{ @board[5] == 10? 'X' : @board[5] }"
+    p " #{ @board[6]== 10? 'X' : @board[6] } | #{@board[7] == 10? 'X' : @board[7]} | #{ @board[8]== 10? 'X' : @board[8] }"
+
+  end
+
+  def reset_board
+    @board = [1, 2, 3, 4, 10, 6, 7, 8, 9]
+    puts 'Board reseted to default'
   end
 
   def update_board(mark, position)
     if verify_hand(position)
       case position
       when 1
-        @board[0][0]=mark
+        @board[0]=mark
       when 2
-        @board[0][1]=mark
+        @board[1]=mark
       when 3
-        @board[0][2]=mark
+        @board[2]=mark
       when 4
-        @board[1][0]=mark
+        @board[3]=mark
       when 5
-        @board[1][1]=mark
+        @board[4]=mark
       when 6
-        @board[1][2]=mark
+        @board[5]=mark
       when 7
-        @board[2][0]=mark
+        @board[6]=mark
       when 8
-        @board[2][1]=mark
+        @board[7]=mark
       when 9
-        @board[2][2]=mark
+        @board[8]=mark
       else
         raise 'That position don\'t exit'
       end
     else
       raise 'The position is already taken'
     end
+    show_board
   end
 
   def verify_hand(position)
-   
-    
-    return false
+
+    @board.each do |i|
+      if i == position
+        true
+      end
+    end
   end
 
 
